d2dfa366e09b3b3524fd8a2c8f1a0732ae9f46e4
Gagan Singh <gagansingh010221@gmail.com>
2026-04-13 18:04:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || 'https://backtask-03yg.onrender.com',
+    origin: process.env.CLIENT_URL || 'https://fronttask-thpu.onrender.com',
     methods: ['GET', 'POST'],
     credentials: true,
   },
